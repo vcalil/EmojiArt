15885e9dd6badb7318f97a646a9b40ef90692a6c
Galil <v.calil@hotmail.com>
2021-09-08 05:07:44 -0700

    Adicionando projeto com model

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Galil on 08/09/21.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EmojiArt/EmojiArtModel.Background.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtModel.Background.swift
+//  EmojiArt
+//
+//  Created by Galil on 08/09/21.
+//
+
+import Foundation
+
+extension EmojiArtModel {
+    
+    enum Background {
+        case blank
+        case url
+        case imageData
+    }
+}
